27567cfd1d36065aed605df30939b31d9609299b
mcostantinof@gmail.com <mcostantinof@gmail.com>
2024-05-13 19:45:29 -0300

    Agregar archivos de proyecto.

---
+++ ObligatorioGimnasio/Models/Maquina.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObligatorioGimnasio
+{
+    public class Maquina
+    {
+        public int Id { get; set; }
+        public int LocalId { get; set; }
+        public DateTime FechaCompra {  get; set; }
+        public decimal PrecioCompra { get; set; }
+        public int VidaUtil {  get; set; }
+        public bool Disponible { get; set; }    
+
+    }
+}
